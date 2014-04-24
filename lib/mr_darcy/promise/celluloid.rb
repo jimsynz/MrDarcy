686833020f208716f8b9ccf9f0b1cb60ed96e6a5
James Harton <james@resistor.io>
2014-04-24 20:14:41 +1200

    Rewrite `Promise::Celluloid` to use an Actor for async. Much betterrer.

--- lib/mr_darcy/promise/celluloid.rb
+++ lib/mr_darcy/promise/celluloid.rb
@@ -3,56 +3,110 @@ require 'celluloid/autostart'
 module MrDarcy
   module Promise
     class Celluloid < Base
+      class Actor
+        include ::Celluloid
+
+        def initialize
+          @complete = false
+        end
+
+        def schedule block
+          ::Celluloid::Future.new &block
+        end
+
+        def set_status status, value
+          case status
+          when :success
+            @success.call value
+          when :failure
+            @failure.call value
+          end
+          signal :complete
+          @complete = true
+        end
+
+        def on_success block
+          @success = block
+        end
+
+        def on_failure block
+          @failure = block
+        end
+
+        def await_completion
+          return if @complete
+          wait :complete
+        end
+      end
 
       def initialize *args
-        @waiting = false
+        @complete = false
+        ensure_actor
+        actor.on_success proc { |v| do_resolve v }
+        actor.on_failure proc { |v| do_reject v }
         super
       end
 
+      def resolve value
+        actor.async.set_status :success, value
+        self
+      end
+
+      def reject value
+        actor.async.set_status :failure, value
+        self
+      end
+
       def result
-        wait_until_not_unresolved
+        wait_until_complete
         value
       end
 
       def final
-        wait_until_not_unresolved
+        wait_until_complete
         self
       end
 
       private
 
-      def notify_waiting
-        condition.signal if @waiting
+      def wait_until_complete
+        return if @complete
+        actor.alive? && actor.await_completion
+        @complete = true
       end
 
-      def schedule_promise &block
-        ::Celluloid::Future.new &block
+      def will_resolve value
+        @complete = true
       end
 
-      def condition
-        @condition ||= ::Celluloid::Condition.new
+      def will_reject value
+        @complete = true
       end
 
-      def wait_until_not_unresolved
-        @waiting = true
-        condition.wait if unresolved?
+      def did_reject value
+        actor.async.terminate
+      end
+
+      def did_resolve value
+        actor.async.terminate
       end
 
       def generate_child_promise
-        ChildPromise.new driver: :thread
+        ChildPromise.new driver: :celluloid
       end
 
-      def state_machine_resolve
-        schedule_promise do
-          super
-        end
+      def ensure_actor
+        @actor ||= Actor.new
       end
 
-      def state_machine_reject
-        schedule_promise do
-          super
-        end
+      def actor
+        @actor
       end
+
+      def schedule_promise &block
+        actor.async.schedule block
+      end
+
     end
   end
 end
