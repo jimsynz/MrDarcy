d2b23e17515967dd15cff96cd838e60cd500ffb3
James Harton <james@resistor.io>
2014-12-28 12:53:39 +1300

    Add docs to `MrDarcy::Context` and replace meta programming with `Forwardable`.

--- lib/mr_darcy/context.rb
+++ lib/mr_darcy/context.rb
@@ -1,11 +1,28 @@
+require 'forwardable'
+
+# This class defines the abstract ability to create DCI roles in MrDarcy.
+# Start by subclassing and adding some roles and actions and away you go.
 module MrDarcy
   class Context
+    extend Forwardable
+
+    def_delegators :deferred, :result, :rejected?, :resolved?, :unresolved?
 
     class << self
+      # Defines a role to be mixed into the roll-player
+      # when this context is initialized.
+      #
+      # See MrDarcy::Role#initialize for argument definitions.
       def role role_name, options={}, &block
         self.roles[role_name] = Role.new(role_name, options, &block)
       end
 
+      # Defines an action that can be performed in this
+      # context.
+      #
+      #  * action_name: essentially the name of the method defined in this class.
+      #
+      # Takes a block with which to define the action.
       def action action_name, &block
         define_method action_name do |*args|
           self.then do |value|
@@ -15,11 +32,19 @@ module MrDarcy
         end
       end
 
+      # A list of available roles in this context.
       def roles
         @roles ||= {}
       end
     end
 
+    # Create an instance of the context.  You must pass in role-players
+    # for all roles defined in this context using a single hash argument
+    # of role names (as symbols) to objects.
+    #
+    # eg:
+    #
+    #   BankTransfer.new money_source: account1, money_destination: account2
     def initialize role_players={}
       @driver   = role_players.delete(:driver) || MrDarcy.driver
       @deferred = Deferred.new(driver: driver) {}
@@ -38,6 +63,7 @@ module MrDarcy
       end
     end
 
+    # See MrDarcy::Promise::Base#then
     def then &block
       deferred.then do |value|
         self.instance_exec(value, &block)
@@ -45,6 +71,7 @@ module MrDarcy
       self
     end
 
+    # See MrDarcy::Promise::Base#fail
     def fail &block
       deferred.fail do |value|
         self.instance_exec(value, &block)
@@ -52,12 +79,7 @@ module MrDarcy
       self
     end
 
-    %w| result rejected? resolved? unresolved? |.map(&:to_sym).each do |method|
-      define_method method do
-        deferred.public_send method
-      end
-    end
-
+    # See MrDarcy::Promise::Base#final
     def final
       deferred.final
       self
